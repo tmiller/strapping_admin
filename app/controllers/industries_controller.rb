45896bacc3bfde8a503a1d3d03ebc5b141ee1401
Tom Miller <jackerran@gmail.com>
2012-05-04 09:25:07 -0500

    Add Bootstrap and Kaminari
      * Add bootstrap-sass and style index page of industries
      * Add kaminari and customize for bootstrap-sass
      * Paginate industries

--- app/controllers/industries_controller.rb
+++ app/controllers/industries_controller.rb
@@ -2,34 +2,19 @@ class IndustriesController < ApplicationController
   # GET /industries
   # GET /industries.json
   def index
-    @industries = Industry.all
-
-    respond_to do |format|
-      format.html # index.html.erb
-      format.json { render json: @industries }
-    end
+    @industries = Industry.page(params[:page]).per(10)
   end
 
   # GET /industries/1
   # GET /industries/1.json
   def show
     @industry = Industry.find(params[:id])
-
-    respond_to do |format|
-      format.html # show.html.erb
-      format.json { render json: @industry }
-    end
   end
 
   # GET /industries/new
   # GET /industries/new.json
   def new
     @industry = Industry.new
-
-    respond_to do |format|
-      format.html # new.html.erb
-      format.json { render json: @industry }
-    end
   end
 
   # GET /industries/1/edit
@@ -42,14 +27,10 @@ class IndustriesController < ApplicationController
   def create
     @industry = Industry.new(params[:industry])
 
-    respond_to do |format|
-      if @industry.save
-        format.html { redirect_to @industry, notice: 'Industry was successfully created.' }
-        format.json { render json: @industry, status: :created, location: @industry }
-      else
-        format.html { render action: "new" }
-        format.json { render json: @industry.errors, status: :unprocessable_entity }
-      end
+    if @industry.save
+      redirect_to @industry, notice: 'Industry was successfully created.'
+    else
+      render action: "new"
     end
   end
 
@@ -58,14 +39,10 @@ class IndustriesController < ApplicationController
   def update
     @industry = Industry.find(params[:id])
 
-    respond_to do |format|
-      if @industry.update_attributes(params[:industry])
-        format.html { redirect_to @industry, notice: 'Industry was successfully updated.' }
-        format.json { head :no_content }
-      else
-        format.html { render action: "edit" }
-        format.json { render json: @industry.errors, status: :unprocessable_entity }
-      end
+    if @industry.update_attributes(params[:industry])
+      redirect_to @industry, notice: 'Industry was successfully updated.'
+    else
+      render action: "edit"
     end
   end
 
@@ -74,10 +51,6 @@ class IndustriesController < ApplicationController
   def destroy
     @industry = Industry.find(params[:id])
     @industry.destroy
-
-    respond_to do |format|
-      format.html { redirect_to industries_url }
-      format.json { head :no_content }
-    end
+    redirect_to industries_url
   end
 end
--- app/models/industry.rb
+++ app/models/industry.rb
@@ -1,3 +1,7 @@
 class Industry < ActiveRecord::Base
   attr_accessible :color, :name
+
+  def to_s
+    name 
+  end
 end
