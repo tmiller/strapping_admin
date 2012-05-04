70853d17d3ffb5c2dd7ba91d5a1f1e663e6db618
Tom Miller <jackerran@gmail.com>
2012-05-03 22:43:24 -0500

    Initial commit

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+StrappingAdmin::Application.config.secret_token = '1dd248c77645cb7aa7f3b4fe4511067c1e42f4efe05f802e4c772853722f6e8030cca6c6252ea744865fac203ff68dfe0b6d09747e7bc1d091d56149ede8c332'
---
+++ config/initializers/session_store.rb
@@ -0,0 +1,8 @@
+# Be sure to restart your server when you modify this file.
+
+StrappingAdmin::Application.config.session_store :cookie_store, key: '_strapping_admin_session'
+
+# Use the database for sessions instead of the cookie-based default,
+# which shouldn't be used to store highly confidential information
+# (create the session table with "rails generate session_migration")
+# StrappingAdmin::Application.config.session_store :active_record_store
