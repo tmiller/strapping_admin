901f48c3e7d074970254d1df7bc061d419ca9e8b
Tom Miller <jackerran@gmail.com>
2012-07-30 12:52:22 -0500

    Add seeds for ease of testing

--- db/seeds.rb
+++ db/seeds.rb
@@ -5,3 +5,7 @@
 #
 #   cities = City.create([{ name: 'Chicago' }, { name: 'Copenhagen' }])
 #   Mayor.create(name: 'Emanuel', city: cities.first)
+
+100.times do |i|
+  Industry.create(name: "Industry #{i}", color: 'Green')
+end
